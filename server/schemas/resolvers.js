2f777d4484f38823c69e7ea6ba6a9576aab8d76e
Jennifer Schlierman <jd9913@gmail.com>
2021-01-04 14:05:09 -0700

    update integration and typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,79 @@
+const { User, Book }=require('../models');
+const { signToken }=require('../utils/auth');
+const { AuthenticationError }=require('apollo-server-express');
+
+const resolvers={
+    Query: {
+        me: async(parent, args, context)=>{
+            if(context.user){
+                const userData=await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+
+                return userData;
+            }
+            throw new AuthenticationError('you must be logged in');
+        },
+       users: async()=>{
+           return User.find()
+           .select('-__v -password')
+           .populate('books');
+            
+        },
+        user: async(parent, { username })=>{
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('books')
+        }
+
+    },
+
+    Mutation: {
+        login: async(parent, { email, password })=>{
+            const user = await User.findOne({ email });
+            if(!user){
+                throw new AuthenticationError('user not found')
+            }
+            const correctPw=await user.isCorrectPawssword(password);
+            if(!correctPw){
+                throw new AuthenticationError('password not correct');
+            }
+
+            const token=signToken(user);
+            return { token, user };
+        },
+
+        addUser: async(parent, args)=>{
+            const user=await User.create(args);
+            const token=signToken(user);
+
+            return { token, user };
+        },
+
+        saveBook: async(parent, { input }, context)=>{
+            if(context.user){
+                const updatedUser=await User.findByIdAndUpdate(
+                    {_id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('you must be logged in')
+        },
+
+        removeBook: async(parent, args, context)=>{
+            if(context.user){
+                const updatedUser=await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedbooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You must be logged in')
+        }
+    }
+};
+
+module.exports=resolvers;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,45 @@
 const express = require('express');
 const path = require('path');
+
+
+//imports ApolloServer
+const { ApolloServer }=require('apollo-server-express');
+
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers }=require('./schemas');
 const db = require('./config/connection');
 
 
-const { ApolloServer }=require('apollo-server-express');
-
-const { typeDefs, resolvers }=require('./schemas');
-
-
-const app = express();
 
 const PORT = process.env.PORT || 3301;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const app = express();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+//create new Apollo server and pass in schema data
 
 const server=new ApolloServer({
   typeDefs,
   resolvers
 });
 
+//integrate Apollo server with Express application as middleware
+server.applyMiddleware({ app });
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
