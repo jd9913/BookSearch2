4a33befd991493a8d415dcc5acb73fd84cf9285e
jd9913 <67403842+jd9913@users.noreply.github.com>
2021-01-06 10:45:18 -0700

    added error handle to db connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
+
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MyLibrary', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: true,
-});
+})
+.then(()=>console.log("DB Connected"))
+.catch(err=>console.error(err));
 
 module.exports = mongoose.connection;
