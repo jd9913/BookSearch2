64eb68d0e8aad43958d7ac428ae6f20099e67c9b
Jennifer Schlierman <jd9913@gmail.com>
2021-01-06 19:30:19 -0700

    updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MyLibrary', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/MyLibrary', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
